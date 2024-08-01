a3b6a64d63c03fa727dfefc274ebd2a253a7e00f
V <lytranvinh.work@gmail.com>
2024-08-01 23:08:53 +0700

    [Upgrade]: build with credentials ok

--- src/gg_auth/main.py
+++ src/gg_auth/main.py
@@ -1,11 +1,24 @@
-
+from .create_credentials import readCredentials
 from .create_credentials import create_credentials
+from googleapiclient.discovery import build
+from google.oauth2.credentials import Credentials
+import json
+
 
 def main():
     """
     Main function for the gg_auth package
     """
-    create_credentials()
+    credentials = readCredentials()
+    if credentials is None:
+        create_credentials()
+        credentials = readCredentials()
     
+    credentials_data = json.loads(credentials)
+    credentials = Credentials(**credentials_data)
+
+    service = build('calendar', 'v3', credentials=credentials)
+
+    print(service)
 if __name__ == '__main__':
     main()
