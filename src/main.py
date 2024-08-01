2c7a1570666d713ea5b5ff98ea3bc8804e402f86
V <lytranvinh.work@gmail.com>
2024-08-02 01:01:42 +0700

    [SAVE]

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 from get_data import get_data
+from gg_auth import google_auth
 
 def main():
     """
         Main function
     """
-    get_data()
+    # get_data()
+    google_auth()
     
 if __name__ == "__main__":
     main()
