59dcce9d0128bf6ba329cbcf370a3974e4183d10
V <lytranvinh.work@gmail.com>
2024-08-03 15:29:00 +0700

    [Upgrade]: Handle export modunl service gg api

--- src/gg_auth/main.py
+++ src/gg_auth/main.py
@@ -31,38 +31,9 @@ def main():
     Main function for the gg_auth package
     """
     get_credentials()
+    service = build("calendar", "v3", credentials=creds)
     
-    try:
-        service = build("calendar", "v3", credentials=creds)
-
-        # Get the current time in ISO 8601 format
-        now = datetime.now(timezone.utc).isoformat()
-        print("Getting the upcoming 10 events")
-        events_result = (
-            service.events()
-            .list(
-                calendarId="primary",
-                timeMin=now,
-                maxResults=10,
-                singleEvents=True,
-                orderBy="startTime",
-            )
-            .execute()
-        )
-        events = events_result.get("items", [])
-
-        if not events:
-            print("No upcoming events found.")
-            return
-
-        # Prints the start and name of the next 10 events
-        for event in events:
-            start = event["start"].get("dateTime", event["start"].get("date"))
-            print(start, event["summary"])
-
-    except HttpError as error:
-        print(f"An error occurred: {error}")
-
+    return service
 
 if __name__ == '__main__':
     main()
