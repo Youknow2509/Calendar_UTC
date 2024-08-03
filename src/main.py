a5516ae73ed06a273e7db0c145f72179831085f3
V <lytranvinh.work@gmail.com>
2024-08-03 15:32:24 +0700

    [SAVE]

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 from get_data import get_data
-from gg_auth import google_auth
+from gg_handle import *
 
 def main():
     """
         Main function
     """
     # get_data()
-    google_auth()
-    
+    # get_all_events()
+    create_event(title='Test create event with api')
 if __name__ == "__main__":
     main()
     
