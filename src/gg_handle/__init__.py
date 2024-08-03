b5483e05b4374a9010ea5405c1ddc44556e52006
V <lytranvinh.work@gmail.com>
2024-08-03 16:37:49 +0700

    [Upgrade]: Add export del all events

--- src/gg_handle/__init__.py
+++ src/gg_handle/__init__.py
@@ -1,5 +1,10 @@
 from .get_events import get_all_events
 from .create_event import create_event
 from .create_event_adv import create_event_adv
+from .delete_all_events import delete_all_events
 
-__all__ = ['get_all_events', 'create_event', 'create_event_adv']
+__all__ = ['get_all_events',
+           'create_event',
+           'create_event_adv',
+           'delete_all_events',
+           ]
