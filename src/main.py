b25967b3f36a34c4474c813336c823f5b4c6c598
V <lytranvinh.work@gmail.com>
2024-08-03 17:51:44 +0700

    [SAVE

--- src/main.py
+++ src/main.py
@@ -7,7 +7,11 @@ def main():
     """
     # get_data()
     # get_all_events()
-    create_event(title='Test create event with api')
+    # create_event(title='Test create event with api')
+    
+    create_event_adv(title='Test create "adv" event with api', days=[2, 3])
+    # delete_all_events()
+    
 if __name__ == "__main__":
     main()
     
